f56c99de0b16565d159ddcfb28cc535936f4ae71
Michael Corbishley <mcorbishley@wisc.edu>
2023-08-06 17:20:21 -0500

    add more mock data and update readme

--- src/main/java/com/example/Payroll/LoadDatabase.java
+++ src/main/java/com/example/Payroll/LoadDatabase.java
@@ -13,11 +13,12 @@ class LoadDatabase {
 
   @Bean
   CommandLineRunner initDatabase(EmployeeRepository repository) {
-
     return args -> {
-      log.info("Preloading " + repository.save(new Employee("Bilbo Baggins", "burglar", 0)));
-      log.info("Preloading " + repository.save(new Employee("Frodo Baggins", "thief", 100000)));
-      log.info("Preloading " + repository.save(new Employee("Michael Corbishley", "engineer", 200000)));
+      log.info("Preloading " + repository.save(new Employee("Bilbo Baggins", "Burglar", 0)));
+      log.info("Preloading " + repository.save(new Employee("Frodo Baggins", "Thief", 100000)));
+      log.info("Preloading " + repository.save(new Employee("Michael Corbishley", "Engineer", 200000)));
+      log.info("Preloading " + repository.save(new Employee("Zachary Corbishley", "Cloud", 200000)));
+      log.info("Preloading " + repository.save(new Employee("Alex Corbishley", "Sales", 200000)));
     };
   }
 }
