f8e4ae0f61b9a9b0096738df8dac782d1f84738e
Michael Corbishley <mcorbishley@wisc.edu>
2023-08-06 19:39:03 -0500

    add cross origin for local api usage

--- src/main/java/com/example/Payroll/EmployeeController.java
+++ src/main/java/com/example/Payroll/EmployeeController.java
@@ -1,6 +1,7 @@
 package com.example.Payroll;
 import java.util.List;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 class EmployeeController {
 
   private final EmployeeRepository repository;
